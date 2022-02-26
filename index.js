73f406718bbaf806f9e99c95fd87c729e6435c94
szabicsoki <szabicsoki@gmail.com>
2022-02-26 09:58:47 +0100

    Start
    Most töltöttem fel a filokat

---
+++ index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+var circle = (getElementById = 'circle');
+
+function move() {
+  circle.style.height = '10px'
+  document.setAttribute("style", "background-color:blue;")
+}
+
